26c1a5607bc7200a2b3b1453dfa283ccdb81d40f
Michael Shilman <shilman@lab80.co>
2016-06-09 10:46:45 +0900

    Styles working, but not as a separate NPM package yet

--- nova-base-components/src/stories/index.js
+++ nova-base-components/src/stories/index.js
@@ -1,3 +1,5 @@
+import '../stylesheets/main.scss'
+
 import '../components/users/UsersName.stories.js'
 import '../components/users/UsersAvatar.stories.js'
 import '../components/comments/CommentsItem.stories.js'
