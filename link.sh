d6c23a87fb265ea0a8510aacf8e6565246a090f7
Michael Shilman <shilman@lab80.co>
2016-06-13 18:21:30 -0700

    Update link script

--- link.sh
+++ link.sh
@@ -3,9 +3,15 @@
 cd nova-core && npm link && cd ..
 cd nova-users && npm link && cd ..
 cd nova-forms && npm link && cd ..
+cd nova-comments && npm link && cd ..
+cd nova-categories && npm link && cd ..
+cd nova-posts && npm link && cd ..
 
 cd nova-base-components
 npm link nova-core
 npm link nova-users
 npm link nova-forms
+npm link nova-comments
+npm link nova-categories
+npm link nova-posts
 cd ..
