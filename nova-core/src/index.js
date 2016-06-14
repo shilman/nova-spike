e3a2d8fd1f932b4f2efde965063aae752f5340fc
Sacha Greif <sacha357@gmail.com>
2016-06-14 11:30:10 +0900

    fix postsItem issues

--- nova-core/src/index.js
+++ nova-core/src/index.js
@@ -1,7 +1,10 @@
 import Meteor from './meteor.js'
+import ModalTrigger from './ModalTrigger.jsx'
 import Telescope from './telescope.js'
+
+console.log(ModalTrigger)
 
 const Messages = {}
 
 export default Telescope
-export { Meteor, Telescope, Messages }
+export { Meteor, Telescope, Messages, ModalTrigger }
